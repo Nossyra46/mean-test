b5e0635550289ee0fdbc1a4c3812ef8953c87c20
Nossyra46 <nossyra@gmail.com>
2016-06-02 16:06:06 +0200

    nouvelles routes

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("Recoucou");
+var express = require('express'), app = express();
+var path = require('path');
+var adminRouter = express.Router();
+
+app.use('/admin', adminRouter);
+
+adminRouter.get('/', function(req, res) {
+ 	res.send('I am the dashboard!');
+ });
+
+adminRouter.get('/users', function(req, res) {
+ res.send('I show all the users!');
+ });
+
+adminRouter.get('/posts', function(req, res) {
+ res.send('I show all the posts!');
+ });
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(1337);
+console.log('1337 is the magic port!');
